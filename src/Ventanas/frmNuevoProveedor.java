604ac9e898bddcd2e9ca26fd633a48a378bd3879
analistascac <analistascac@gmail.com>
2014-06-21 19:03:14 -0300

    Modificaciones generales
    No es mas redimencionable, cierra correctamente y muestra mensaje del objeto que va a pasar a la base de datos

--- src/Ventanas/frmNuevoProveedor.java
+++ src/Ventanas/frmNuevoProveedor.java
@@ -24,10 +24,13 @@ import javax.swing.JList;
 
 import Clases.Auxiliar;
 import Clases.Proveedor;
+import Main.Main;
 
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class frmNuevoProveedor extends JFrame {
 
@@ -49,67 +52,26 @@ public class frmNuevoProveedor extends JFrame {
 	private JTextField txtEmail;
 	
 	public frmNuevoProveedor() {
-		setTitle("Nuevo Proveedor");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 443, 305);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent arg0) {
+				Main v = new Main();
+				v.setVisible(true);
+				dispose();
+			}
+		});
+		setTitle("Nuevo Proveedor - Veterinaria CAC");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 419, 305);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		txtDireccion = new JTextField();
-		txtDireccion.setColumns(10);
-		txtDireccion.setBounds(141, 75, 246, 23);
-		contentPane.add(txtDireccion);
-		
-		txtTelefono = new JTextField();
-		txtTelefono.setColumns(10);
-		txtTelefono.setBounds(141, 109, 246, 23);
-		contentPane.add(txtTelefono);
-		
 		lblDireccion = new JLabel("Direccion");
 		lblDireccion.setHorizontalAlignment(SwingConstants.RIGHT);
 		lblDireccion.setBounds(55, 77, 72, 21);
 		contentPane.add(lblDireccion);
-		
-		txtFax = new JTextField();
-		txtFax.setColumns(10);
-		txtFax.setBounds(141, 143, 246, 21);
-		contentPane.add(txtFax);
-		
-		btnAceptar = new JButton("Aceptar");
-		btnAceptar.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				//validaciones
-				
-				ArrayList<String> errores=new ArrayList<String>();
-				if(!Auxiliar.isValidCUIT(txtCUIT.getText()))errores.add("Numero de CUIT invalido.");
-				if(!Auxiliar.isValidTelephone(txtTelefono.getText()))errores.add("Numero de Telefono invalido.");
-				if(!Auxiliar.isValidTelephone(txtFax.getText()))errores.add("Numero de Fax invalido.");
-				if(!Auxiliar.isValidEmail(txtEmail.getText()))errores.add("Email invalido.");
-				//si hay errores los muestra
-				if(errores.size()>0) JOptionPane.showMessageDialog(null,Auxiliar.contenarArrayList(errores));
-				else
-				{
-					//si no hubo errores,crea el objeto proveedor
-					Proveedor pv=new Proveedor();
-					pv.setCuit(txtCUIT.getText());
-					pv.setEmail(txtEmail.getText());
-					pv.setDireccion(txtDireccion.getText());
-					pv.setFax(txtFax.getText());
-					pv.setTelefono(txtTelefono.getText());
-					pv.setRazon_social(txtRazonSocial.getText());
-				}
-				
-				
-			}
-		});
-		btnAceptar.setBounds(267, 233, 89, 23);
-		contentPane.add(btnAceptar);
-		
-		btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(153, 233, 89, 23);
-		contentPane.add(btnCancelar);
 		
 		lblTelefono = new JLabel("Telefono");
 		lblTelefono.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -126,30 +88,82 @@ public class frmNuevoProveedor extends JFrame {
 		lblCUIT.setBounds(24, 45, 107, 21);
 		contentPane.add(lblCUIT);
 		
-		txtCUIT = new JTextField();
-		txtCUIT.setColumns(10);
-		txtCUIT.setBounds(141, 45, 246, 21);
-		contentPane.add(txtCUIT);
-		
 		txtNombre = new JLabel("Razon social");
 		txtNombre.setHorizontalAlignment(SwingConstants.RIGHT);
 		txtNombre.setBounds(24, 12, 107, 21);
 		contentPane.add(txtNombre);
+		
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblEmail.setBounds(20, 175, 107, 21);
+		contentPane.add(lblEmail);
 		
 		txtRazonSocial = new JTextField();
 		txtRazonSocial.setColumns(10);
 		txtRazonSocial.setBounds(141, 11, 246, 23);
 		contentPane.add(txtRazonSocial);
 		
+		txtCUIT = new JTextField();
+		txtCUIT.setColumns(10);
+		txtCUIT.setBounds(141, 45, 246, 21);
+		contentPane.add(txtCUIT);
+		
+		txtDireccion = new JTextField();
+		txtDireccion.setColumns(10);
+		txtDireccion.setBounds(141, 75, 246, 23);
+		contentPane.add(txtDireccion);
+		
+		txtTelefono = new JTextField();
+		txtTelefono.setColumns(10);
+		txtTelefono.setBounds(141, 109, 246, 23);
+		contentPane.add(txtTelefono);
+		
+		txtFax = new JTextField();
+		txtFax.setColumns(10);
+		txtFax.setBounds(141, 143, 246, 21);
+		contentPane.add(txtFax);
+		
 		txtEmail = new JTextField();
 		txtEmail.setColumns(10);
 		txtEmail.setBounds(141, 172, 246, 21);
 		contentPane.add(txtEmail);
 		
-		JLabel lblEmail = new JLabel("Email");
-		lblEmail.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblEmail.setBounds(20, 175, 107, 21);
-		contentPane.add(lblEmail);
+		btnAceptar = new JButton("Aceptar");
+		btnAceptar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				//validaciones
+				
+				ArrayList<String> errores=new ArrayList<String>();
+				if(!Auxiliar.isValidCUIT(txtCUIT.getText()))errores.add("Numero de CUIT invalido.");
+				if(!Auxiliar.isValidTelephone(txtTelefono.getText()))errores.add("Numero de Telefono invalido.");
+				if(!Auxiliar.isValidTelephone(txtFax.getText()))errores.add("Numero de Fax invalido.");
+				if(!Auxiliar.isValidEmail(txtEmail.getText()))errores.add("Email invalido.");
+				//si hay errores los muestra
+				if(errores.size()>0) JOptionPane.showMessageDialog(null,Auxiliar.contenarArrayList(errores));
+				else
+				{
+					//si no hubo errores,crea el objeto proveedor
+					
+					Proveedor pv=new Proveedor();
+					pv.setCuit(txtCUIT.getText());
+					pv.setEmail(txtEmail.getText());
+					pv.setDireccion(txtDireccion.getText());
+					pv.setFax(txtFax.getText());
+					pv.setTelefono(txtTelefono.getText());
+					pv.setRazon_social(txtRazonSocial.getText());
+					
+					JOptionPane.showMessageDialog(null, pv.toString());
+				}
+				
+				
+			}
+		});
+		btnAceptar.setBounds(267, 233, 89, 23);
+		contentPane.add(btnAceptar);
+		
+		btnCancelar = new JButton("Cancelar");
+		btnCancelar.setBounds(153, 233, 89, 23);
+		contentPane.add(btnCancelar);
 		
 		
 	}
