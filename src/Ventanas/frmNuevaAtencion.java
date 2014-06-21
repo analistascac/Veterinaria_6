973fac688f3b3bde0be5d5a40c6cb61d7ef3218f
analistascac <analistascac@gmail.com>
2014-06-21 18:57:18 -0300

    Modificaciones generales
    Arregle todos los modal, ya no se pueden redimencionar, se cierra correctamente sin cerrar todo, ordene las cosas para que se pueda tabular bien, puse un mensaje en el boton aceptar apra ver como quedaria el objeto que va a base de datos..

--- src/Ventanas/frmNuevaAtencion.java
+++ src/Ventanas/frmNuevaAtencion.java
@@ -26,18 +26,21 @@ import java.util.ArrayList;
 import javax.swing.JTextArea;
 
 import Clases.Atencion;
+import Main.Main;
 import Main.TFecha;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class frmNuevaAtencion extends JFrame {
 
 	private JPanel contentPane;
-	private JLabel lblTipoDoc;
+	private JLabel lblTipoConsulta;
 	private JLabel lblDiagnostico;
 	private JLabel lblFecha;
 	private JLabel lblDia;
 	private JLabel lblMes;
 	private JLabel lblAnio;
-	private DefaultComboBoxModel tipodoc = null;
 	private JComboBox cmbDia;
 	private JComboBox cmbMes;
 	private JComboBox cmbAnio;
@@ -46,16 +49,27 @@ public class frmNuevaAtencion extends JFrame {
 	private JButton btnAceptar;
 	private TFecha fecha;
 	private JComboBox cmbCliente;
+	private DefaultComboBoxModel clientes = new DefaultComboBoxModel();
 	private JComboBox cmbVeterinario;
+	private DefaultComboBoxModel veterinarios = new DefaultComboBoxModel();
 	private JComboBox cmbMascota;
+	private DefaultComboBoxModel mascotas = new DefaultComboBoxModel();
 	private JTextArea txtDiagnostico;
-	
 	
 	@SuppressWarnings("rawtypes")
 	public frmNuevaAtencion() {
-		setTitle("Nueva Atencion");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 461, 319);
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosed(WindowEvent arg0) {
+				Main v = new Main();
+				v.setVisible(true);
+				dispose();
+			}
+		});
+		setResizable(false);
+		setTitle("Nueva Atencion - Veterinaria CAC");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setBounds(100, 100, 422, 319);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -71,15 +85,15 @@ public class frmNuevaAtencion extends JFrame {
 		lblVeterinario.setBounds(35, 43, 96, 21);
 		contentPane.add(lblVeterinario);
 		
-		lblTipoDoc = new JLabel("Tipo de consulta");
-		lblTipoDoc.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblTipoDoc.setBounds(10, 75, 120, 21);
-		contentPane.add(lblTipoDoc);
+		lblTipoConsulta = new JLabel("Tipo de consulta");
+		lblTipoConsulta.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblTipoConsulta.setBounds(10, 75, 120, 21);
+		contentPane.add(lblTipoConsulta);
 		
-		cmbTipoConsulta = new JComboBox();
-		cmbTipoConsulta.setModel(new DefaultComboBoxModel(new String[] {"Consulta General", "Operacion", "Aplicacion de medicamentos"}));
-		cmbTipoConsulta.setBounds(151, 75, 114, 21);
-		contentPane.add(cmbTipoConsulta);
+		JLabel lblMascota = new JLabel("Mascota");
+		lblMascota.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblMascota.setBounds(13, 102, 120, 21);
+		contentPane.add(lblMascota);
 		
 		lblDiagnostico = new JLabel("Diagnostico");
 		lblDiagnostico.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -87,7 +101,7 @@ public class frmNuevaAtencion extends JFrame {
 		contentPane.add(lblDiagnostico);
 		
 		lblFecha = new JLabel("Fecha");
-		lblFecha.setBounds(35, 180, 131, 21);
+		lblFecha.setBounds(10, 181, 131, 21);
 		contentPane.add(lblFecha);
 		
 		lblDia = new JLabel("Dia");
@@ -98,9 +112,37 @@ public class frmNuevaAtencion extends JFrame {
 		lblMes.setBounds(139, 213, 23, 21);
 		contentPane.add(lblMes);
 		
-		lblAnio = new JLabel("Año");
+		lblAnio = new JLabel("AÃ±o");
 		lblAnio.setBounds(282, 213, 23, 21);
 		contentPane.add(lblAnio);
+		
+		cmbCliente = new JComboBox();
+		cmbCliente.setModel(clientes);
+		cargarClientes();
+		cmbCliente.setBounds(150, 11, 247, 21);
+		contentPane.add(cmbCliente);
+		
+		cmbVeterinario = new JComboBox();
+		cmbVeterinario.setModel(veterinarios);
+		cargarVeterinarios();
+		cmbVeterinario.setBounds(150, 43, 247, 21);
+		contentPane.add(cmbVeterinario);
+		
+		cmbTipoConsulta = new JComboBox();
+		cmbTipoConsulta.setModel(new DefaultComboBoxModel(new String[] {"Consulta General", "Operacion", "Aplicacion de medicamentos"}));
+		cmbTipoConsulta.setBounds(151, 75, 246, 21);
+		contentPane.add(cmbTipoConsulta);
+		
+		cmbMascota = new JComboBox();
+		cmbMascota.setModel(mascotas);
+		cargarMascotas();
+		cmbMascota.setBounds(151, 104, 246, 21);
+		contentPane.add(cmbMascota);
+		
+		txtDiagnostico = new JTextArea();
+		txtDiagnostico.setLineWrap(true);
+		txtDiagnostico.setBounds(151, 134, 246, 53);
+		contentPane.add(txtDiagnostico);
 		
 		cmbDia = new JComboBox();
 		cmbDia.setBounds(35, 213, 82, 21);
@@ -111,59 +153,50 @@ public class frmNuevaAtencion extends JFrame {
 		contentPane.add(cmbMes);
 		
 		cmbAnio = new JComboBox();
-		cmbAnio.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				
-			}
-		});
 		cmbAnio.setBounds(315, 213, 82, 21);
 		contentPane.add(cmbAnio);
+		
+		fecha = new TFecha(cmbDia,cmbMes, cmbAnio);
 		
 		btnAceptar = new JButton("Aceptar");
 		btnAceptar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-			Atencion atenc=new Atencion();
-			atenc.setCliente((String) cmbCliente.getSelectedItem());
-			atenc.setVeterinario((String) cmbVeterinario.getSelectedItem());
-			atenc.setMascota((String) cmbMascota.getSelectedItem());
-			atenc.setTipo_consulta((String) cmbTipoConsulta.getSelectedItem());
-			atenc.setDiagnostico(txtDiagnostico.getText());
-			
+				Atencion atenc=new Atencion();
+				atenc.setCliente((String) cmbCliente.getSelectedItem());
+				atenc.setVeterinario((String) cmbVeterinario.getSelectedItem());
+				atenc.setMascota((String) cmbMascota.getSelectedItem());
+				atenc.setTipo_consulta((String) cmbTipoConsulta.getSelectedItem());
+				atenc.setDiagnostico(txtDiagnostico.getText());
+				atenc.setFecha(fecha.getFechaString());
+				
+				JOptionPane.showMessageDialog(null, atenc.toString(),"Informacion",JOptionPane.INFORMATION_MESSAGE);
 				
 			}
 		});
-		btnAceptar.setBounds(298, 244, 89, 23);
+		btnAceptar.setBounds(308, 245, 89, 23);
 		contentPane.add(btnAceptar);
 		
 		btnCancelar = new JButton("Cancelar");
-		btnCancelar.setBounds(192, 244, 89, 23);
+		btnCancelar.setBounds(209, 245, 89, 23);
 		contentPane.add(btnCancelar);
 		
-		fecha = new TFecha(cmbDia,cmbMes, cmbAnio);
 		
-		cmbCliente = new JComboBox();
-		cmbCliente.setModel(new DefaultComboBoxModel(new String[] {"-", "John Doe", "Marcos mercury", "Sebastian tahl"}));
-		cmbCliente.setBounds(150, 11, 115, 21);
-		contentPane.add(cmbCliente);
-		
-		cmbVeterinario = new JComboBox();
-		cmbVeterinario.setModel(new DefaultComboBoxModel(new String[] {"-", "Carlos Estevez", "Jose Peralta"}));
-		cmbVeterinario.setBounds(150, 43, 115, 21);
-		contentPane.add(cmbVeterinario);
-		
-		JLabel lblMascota = new JLabel("Mascota");
-		lblMascota.setHorizontalAlignment(SwingConstants.RIGHT);
-		lblMascota.setBounds(13, 102, 120, 21);
-		contentPane.add(lblMascota);
-		
-		cmbMascota = new JComboBox();
-		cmbMascota.setModel(new DefaultComboBoxModel(new String[] {"-", "Scott", "Homero"}));
-		cmbMascota.setBounds(151, 104, 115, 21);
-		contentPane.add(cmbMascota);
-		
-		txtDiagnostico = new JTextArea();
-		txtDiagnostico.setBounds(151, 134, 200, 50);
-		contentPane.add(txtDiagnostico);
+	}
+	
+	private void cargarMascotas(){
+		//tiene que recibir las mascotas del storeprocedure y agregarlas a la variable mascotas.
+		mascotas.addElement("Mascota prueba 1");
+		mascotas.addElement("Mascota prueba 2");
+		mascotas.addElement("Mascota prueba 3");
+	}
+	private void cargarVeterinarios(){
+		veterinarios.addElement("Veterinario prueba 1");
+		veterinarios.addElement("Veterinario prueba 2");
+		veterinarios.addElement("Veterinario prueba 3");	
+	}
+	private void cargarClientes(){
+		clientes.addElement("Cliente prueba 1");
+		clientes.addElement("Cliente prueba 2");
+		clientes.addElement("Cliente prueba 3");
 	}
 }
